9f028b71daf471ac2374ac7110196c0cf6cefb0f
Said İmamoğlu <said.imamoglu@biberltd.com>
2014-09-09 15:40:14 +0300

    Bundle upgraded to 3.1

---
+++ BiberLtdAwardManagementBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BiberLtd\Bundle\AwardManagementBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BiberLtdAwardManagementBundle extends Bundle
+{
+}
---
+++ Entity/AwardLocalization.php
@@ -0,0 +1,327 @@
+<?php
+/**
+ * @name        AwardLocalization
+ * @package		BiberLtd\Core\AwardManagementBundle
+ *
+ * @author		Murat Ünal
+ *
+ * @version     1.0.0
+ * @date        20.09.2013
+ *
+ * @copyright   Biber Ltd. (http://www.biberltd.com)
+ * @license     GPL v3.0
+ *
+ * @description Model / Entity class.
+ *
+ */
+namespace BiberLtd\Core\Bundles\AwardManagementBundle\Entity;
+use Doctrine\ORM\Mapping AS ORM;
+use BiberLtd\Core\CoreEntity;
+
+/** 
+ * @ORM\Entity
+ * @ORM\Table(
+ *     name="award_localization",
+ *     options={"charset":"utf8","collate":"utf8_turkish_ci","engine":"innodb"},
+ *     uniqueConstraints={@ORM\UniqueConstraint(name="idx_u_award_localization", columns={"award","language"})}
+ * )
+ */
+class AwardLocalization extends CoreEntity
+{
+    /** 
+     * @ORM\Column(type="string", length=155, nullable=false)
+     */
+    private $title;
+
+    /** 
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private $url_key;
+
+    /** 
+     * @ORM\Column(type="time", nullable=true)
+     */
+    private $content;
+
+    /** 
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $summary;
+
+    /** 
+     * @ORM\Id
+     * @ORM\ManyToOne(
+     *     targetEntity="BiberLtd\Core\Bundles\AwardManagementBundle\Entity\Award",
+     *     inversedBy="localizations"
+     * )
+     * @ORM\JoinColumn(name="award", referencedColumnName="id", nullable=false, onDelete="CASCADE")
+     */
+    private $award;
+
+    /** 
+     * @ORM\Id
+     * @ORM\ManyToOne(targetEntity="BiberLtd\Core\Bundles\MultiLanguageSupportBundle\Entity\Language")
+     * @ORM\JoinColumn(name="language", referencedColumnName="id", nullable=false, onDelete="CASCADE")
+     */
+    private $language;
+
+    /**
+     * @name                  setAward ()
+     *                                 Sets the award property.
+     *                                 Updates the data only if stored value and value to be set are different.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @use             $this->setModified()
+     *
+     * @param           mixed $award
+     *
+     * @return          object                $this
+     */
+    public function setAward($award) {
+        if(!$this->setModified('award', $award)->isModified()) {
+            return $this;
+        }
+		$this->award  =  $award;
+		return $this;
+    }
+
+    /**
+     * @name            getAward ()
+     *                           Returns the value of award property.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @return          mixed           $this->award
+     */
+    public function getAward() {
+        return $this->award;
+    }
+
+    /**
+     * @name                  setContent ()
+     *                                   Sets the content property.
+     *                                   Updates the data only if stored value and value to be set are different.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @use             $this->setModified()
+     *
+     * @param           mixed $content
+     *
+     * @return          object                $this
+     */
+    public function setContent($content) {
+        if(!$this->setModified('content', $content)->isModified()) {
+            return $this;
+        }
+		$this->content  =  $content;
+		return $this;
+    }
+
+    /**
+     * @name            getContent ()
+     *                             Returns the value of content property.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @return          mixed           $this->content
+     */
+    public function getContent() {
+        return $this->content;
+    }
+
+    /**
+     * @name                  setLanguage ()
+     *                                    Sets the language property.
+     *                                    Updates the data only if stored value and value to be set are different.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @use             $this->setModified()
+     *
+     * @param           mixed $language
+     *
+     * @return          object                $this
+     */
+    public function setLanguage($language) {
+        if(!$this->setModified('language', $language)->isModified()) {
+            return $this;
+        }
+		$this->language  =  $language;
+		return $this;
+    }
+
+    /**
+     * @name            getLanguage ()
+     *                              Returns the value of language property.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @return          mixed           $this->language
+     */
+    public function getLanguage() {
+        return $this->language;
+    }
+
+    /**
+     * @name                  setSummary ()
+     *                                   Sets the summary property.
+     *                                   Updates the data only if stored value and value to be set are different.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @use             $this->setModified()
+     *
+     * @param           mixed $summary
+     *
+     * @return          object                $this
+     */
+    public function setSummary($summary) {
+        if(!$this->setModified('summary', $summary)->isModified()) {
+            return $this;
+        }
+		$this->summary  =  $summary;
+		return $this;
+    }
+
+    /**
+     * @name            getSummary ()
+     *                             Returns the value of summary property.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @return          mixed           $this->summary
+     */
+    public function getSummary() {
+        return $this->summary;
+    }
+
+    /**
+     * @name                  setTitle ()
+     *                                 Sets the title property.
+     *                                 Updates the data only if stored value and value to be set are different.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @use             $this->setModified()
+     *
+     * @param           mixed $title
+     *
+     * @return          object                $this
+     */
+    public function setTitle($title) {
+        if(!$this->setModified('title', $title)->isModified()) {
+            return $this;
+        }
+		$this->title  =  $title;
+		return $this;
+    }
+
+    /**
+     * @name            getTitle ()
+     *                           Returns the value of title property.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @return          mixed           $this->title
+     */
+    public function getTitle() {
+        return $this->title;
+    }
+
+    /**
+     * @name                  setUrlKey ()
+     *                                  Sets the url_key property.
+     *                                  Updates the data only if stored value and value to be set are different.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @use             $this->setModified()
+     *
+     * @param           mixed $url_key
+     *
+     * @return          object                $this
+     */
+    public function setUrlKey($url_key) {
+        if(!$this->setModified('url_key', $url_key)->isModified()) {
+            return $this;
+        }
+		$this->url_key  =  $url_key;
+		return $this;
+    }
+
+    /**
+     * @name            getUrlKey ()
+     *                            Returns the value of url_key property.
+     *
+     * @author          Can Berkol
+     *
+     * @since           1.0.0
+     * @version         1.0.0
+     *
+     * @return          mixed           $this->url_key
+     */
+    public function getUrlKey() {
+        return $this->url_key;
+    }
+    /******************************************************************
+     * PUBLIC SET AND GET FUNCTIONS                                   *
+     ******************************************************************/
+
+}
+/**
+ * Change Log:
+ * **************************************
+ * v1.0.0                      Murat Ünal
+ * 20.09.2013
+ * **************************************
+ * A getAward()
+ * A getContent()
+ * A getLanguage()
+ * A getSummary()
+ * A getTitle()
+ * A getUrlKey()
+ *
+ * A setAward()
+ * A setContent()
+ * A setLanguage()
+ * A setSummary()
+ * A setTitle()
+ * A setTitle()
+ * A setUrlKey()
+ *
+ */
